9573072e646194d0e570543fd863171529f14b52
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-12-03 08:41:50 +0100

    :zap: Base API

---
+++ source/Core/Api.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Source\Core;
+
+/**
+ * Class Api
+ * @package Source\Core
+ */
+class Api
+{
+    /** @var $response */
+    protected $response;
+
+    /**
+     * @param int $code
+     * @param bool|null $success
+     * @param string|null $message
+     * @return Api|null
+     */
+    protected function call(int $code, bool $success, ?string $message = null): ?Api
+    {
+        http_response_code($code);
+
+        $this->response = [
+            "success" => $success,
+            "message" => $message ?: null,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * @param array|null $data
+     * @return Api
+     */
+    protected function back(array $data = null): Api
+    {
+        if (!empty($this->response)) {
+            $this->response = ($data ? array_merge($this->response, $data) : $this->response);
+            echo json_encode($this->response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+            return $this;
+        }
+
+        echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+        return $this;
+    }
+}
