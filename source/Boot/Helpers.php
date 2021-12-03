d30a3f94a3a5584564d5dd7e9d1cd1913eb3b0b4
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-12-03 08:43:39 +0100

    :zap: URL function

--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -1,6 +1,32 @@
 <?php
 
 /**
+ * ################
+ * ###   URLs   ###
+ * ################
+ */
+
+/**
+ * @param string|null $path
+ *
+ * @return string
+ */
+function url(string $path = null): string
+{
+    if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
+        if ($path) {
+            return CONF_URL_TEST . "/" . ($path[0] == "/" ? substr($path, 1) : $path);
+        }
+        return CONF_URL_TEST;
+    }
+
+    if ($path) {
+        return CONF_URL_BASE . "/" . ($path[0] == "/" ? substr($path, 1) : $path);
+    }
+    return CONF_URL_BASE;
+}
+
+/**
  * ####################
  * ###   VALIDATE   ###
  * ####################
