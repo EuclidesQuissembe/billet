5b09a83ccb987e80c1ba2cffe76608443f007af2
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-12-04 00:58:54 +0100

    :zap: Users create

---
+++ source/Controllers/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Source\Controllers;
+
+use Source\Core\Auth;
+
+class Users extends RootApi
+{
+    public function me(): void
+    {
+        if (!Auth::user()) {
+            $this->call(
+                500,
+                false,
+                "Precisas estar autenticado para ter acesso"
+            )->back();
+            return;
+        }
+
+        $user = Auth::user();
+
+        unset($user->data()->password_hash);
+
+        $this->call(200, true)->back($user->data());
+    }
+}
